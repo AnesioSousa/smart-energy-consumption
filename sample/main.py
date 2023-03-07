0c17c4a787294a434dded7f73bdaeff911a64c73
Anésio Neto <anesios98@gmail.com>
2023-03-07 15:19:25 -0300

    Bug fix on file server

--- sample/main.py
+++ sample/main.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python3
+
 from server import Server
 from sensor import Sensor
-from controller.httpParser import HttpParser
+from controllers.httpParser import HttpParser
 import threading
+import http.server
+import socketserver
 
 if __name__ == '__main__':
 
-    my_sensor = Sensor('localhost', 65301)
-    my_sensor.connect()
     my_server = Server('localhost', 65301)
     my_server.start_listening()
+
+    parser = HttpParser()
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", 65301), Handler) as httpd:
+        print("serving at port", 65301)
+        httpd.serve_forever()
